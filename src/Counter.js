0a3a90cdbce7d7a35319f0be225aa8ad5213c997
andrek <zblackoez@gmail.com>
2023-04-30 03:01:36 -0400

    Envio

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Counter.css'
+function Counter() {
+  return (
+    <div className="counter">
+    <span>5</span>
+    <button>-</button>
+    <button>+</button>
+    </div>
+  )
+
+}
+
+export default Counter;
