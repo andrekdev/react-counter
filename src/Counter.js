e8e91981d479109c3fca657403b64a94e974895b
andrek <zblackoez@gmail.com>
2023-04-30 03:19:18 -0400

    Manipulando o estado de um componente

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState} from "react";
+//useState gerenciar estado de compontente, no caso, mudar o numero do span
+
 import './Counter.css'
-function Counter() {
+const Counter = () => {
+  //Para eventos de clique, sempre tera de ser onClick
+
+  //let contador = 0
+  const [contador, setContador] = useState(0)
+
+  //setContador vai ser usado para mudar o estado do numero, setContador = 1
+
+
+  //funcao do onClick
+
+  const eventoClique = operador => {
+    //tende criar outra variavel, pois o setContador é const
+    const novoValor = operador === '+' ? contador + 1
+    : contador - 1
+    
+    setContador(novoValor)
+  }
+
   return (
+
     <div className="counter">
-    <span>5</span>
-    <button>-</button>
-    <button>+</button>
+    <span>{contador}</span>
+    <button onClick={() => eventoClique('-')}>-</button>
+    <button onClick={() => eventoClique('+')}>+</button>
     </div>
+
   )
 
 }
