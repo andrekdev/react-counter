9e254516261a25b0a20f7a314120159e0a243d78
andrek <zblackoez@gmail.com>
2023-05-02 04:01:00 -0400

    Usando o useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,26 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 //useState gerenciar estado de compontente, no caso, mudar o numero do span
 
 import './Counter.css'
 const Counter = () => {
+
+
   //Para eventos de clique, sempre tera de ser onClick
 
   //let contador = 0
   const [contador, setContador] = useState(0)
+
+
+  //UseEffect
+  //meio que um aviso, caso altere a variavel
+
+
+  useEffect(() => {
+    console.log("Renderizou")
+  }, [
+    //ele depende de uma variavel, se ela se alterar ele faz o console.log
+    contador
+  ])
 
   //setContador vai ser usado para mudar o estado do numero, setContador = 1
 
